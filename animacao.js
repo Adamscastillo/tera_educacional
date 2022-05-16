8db559fbacb3b34dabb9f3f6d3bd21baa64c7f9f
Adams Castillo <102859028+Adamscastillo@users.noreply.github.com>
2022-05-16 17:18:43 -0300

    Add files via upload

---
+++ animacao.js
@@ -0,0 +1,16 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+  idx++;
+  if(idx > img.length - 1 ){
+    idx = 0
+  }
+
+  imgs.style.transform = `translatex(${-idx * 428 }px)`;
+
+}
+
+setInterval(carrossel, 1800);
